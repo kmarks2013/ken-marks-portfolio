ebc515fd12af24e599825ca9451a34f2bc80c47b
kmarks2013 <kmarks2013@gmail.com>
2020-07-08 15:06:36 -0400

    orders imports in oder of render

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AboutMe } from '../components/AboutMe'
-import { Weblinks } from '../components/Weblinks'
 import { TwitterFeed } from '../components/TwitterFeed'
+import { Weblinks } from '../components/Weblinks'
 
 export const MainContainer = () => {
     return (
