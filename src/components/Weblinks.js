7e858a528ce6ab613cd987ddb7216551a2964913
kmarks2013 <kmarks2013@gmail.com>
2020-07-08 14:43:46 -0400

    adds links and icons to weblink file

--- src/components/Weblinks.js
+++ src/components/Weblinks.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import 'font-awesome/css/font-awesome.min.css'
 
 export const Weblinks = () => {
     return (
         <div className='weblinks'>
-            <p>LinkedIn</p>
-            <p>Github</p>
-            <p>Twitter</p>
+            <a href="https://www.linkedin.com/in/kmarks2013/">
+            <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+            <a href="https://github.com/kmarks2013">
+            <i className="fa fa-github" aria-hidden="true"></i>
+            </a>
+            <a href="https://twitter.com/kmarks2013">
+            <i className="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+            <a href="https://medium.com/kmarks2013">
             <i className="fa fa-medium"></i>
-            <p>Medium</p>
+            </a>
         </div>
     )
 }
