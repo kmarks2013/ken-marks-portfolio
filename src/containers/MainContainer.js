f4e388f460a851539278a024f9c1f7ecaa96b558
kmarks2013 <kmarks2013@gmail.com>
2020-07-08 15:17:23 -0400

    renders portfolio component on main container

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { AboutMe } from '../components/AboutMe'
 import { TwitterFeed } from '../components/TwitterFeed'
 import { Weblinks } from '../components/Weblinks'
+import { Portfolio } from '../components/Portfolio'
 
 export const MainContainer = () => {
     return (
         <div>
             <AboutMe/>
+            <Portfolio />
             <TwitterFeed />
             <Weblinks />
         </div>
