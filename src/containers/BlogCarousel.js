3f4ec2707322b6f66a0e703f5b2882e7b4bd230e
kmarks2013 <kmarks2013@gmail.com>
2020-07-08 19:38:52 -0400

    removes things from fetch" git add -p

--- src/containers/BlogCarousel.js
+++ src/containers/BlogCarousel.js
@@ -25,19 +25,12 @@ export const Blogs = () => {
 
     const apiFetch = () => {
         fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kmarks2013")
-            .then(response => {
-                if (!response.ok) {
-                    throw Error("Network request failed")
-                }
-                return response
-                })
             .then((res) => res.json())
             .then((data) => {
                 // console.log(data.items)
                 const res = data.items
                 const posts = res.filter(item => item.categories.length > 0)
                 setMediumPosts(posts)
-                setRequestFailed(true)
             })
     }
 
