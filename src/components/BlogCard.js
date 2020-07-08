a4753ea86801dfea77603ac0a6ce66f971fa9f31
kmarks2013 <kmarks2013@gmail.com>
2020-07-08 19:40:49 -0400

    updates blog card to render apropirate information

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React from 'react'
+import data from '../data'
 
-const tagToText = (node) => {
-  let tag = document.createElement('div')
-  tag.innerHTML = node
-  node = tag.innerText
-  return node
-}
-const shortenText = (text,startingPoint ,maxLength) => {
-  return text.length > maxLength?
-  text.slice(startingPoint, maxLength):
-  text
-}
-function Card(props) {
-    return(
-        <div className="card">
-            <img src={props.post.thumbnail} className="card-img-top post-thumbnail" alt={props.post.title}></img>
-            <div className="card-body">
-                <h5 className="card-title post-title">{props.post.title}</h5>
-                <p className="card-text post-preview">{'...' + shortenText(tagToText(props.post.content), 60, 200) + '...'}</p>
-                <a href={props.post.link} className="btn btn-link-grey">Read this article on Medium.com</a>
-            </div>
-        </div>
+
+export default function BlogCard({active, mediumPosts}) {
+    const tagToText = (node) => {
+        let tag = document.createElement('div')
+        tag.innerHTML = node
+        node = tag.innerText
+        return node
+      }
+
+      const shortenText = (text,startingPoint ,maxLength) => {
+        return text.length > maxLength?
+        text.slice(startingPoint, maxLength):
+        text
+      }
+
+    return (
+        <section>
+            {
+                mediumPosts.map((s, index) =>
+                    <div className={index === active ? 'active' : 'inactive'} key={index}>
+                        <img src={s.thumbnail} style={{width:'50px'}}/>
+                        <div>
+                        <h5 className='card-title post-title'>{s.title}</h5>
+                        {/* <p>{s.description}</p> */}
+                        <p className='card-text post-preview'> {'..' + `${shortenText(tagToText(s.description), 60, 200)}` + '...'}</p>
+                        <a href={s.link} className="btn btn-link-grey">Read this article on Medium.com</a>
+                        </div>
+                    </div>
+                )
+            }
+       </section>
     )
 }
-export default Card
