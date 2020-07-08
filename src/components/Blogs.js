fdc6c9b15b57dde65f0f6b57865103ebd71dfa7d
kmarks2013 <kmarks2013@gmail.com>
2020-07-08 18:22:22 -0400

    adds fetch for blogposts and adds them to  state

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect}  from 'react'
+import BlogCard from './BlogCard'
 
 export const Blogs = () => {
+    const [requestFailed, setRequestFailed] = useState(false)
+    const [active, setActive] = useState(0)
+    const [mediumPosts, setMediumPosts] = useState([])
+
+    const apiFetch = () => {
+        fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kmarks2013")
+            .then(response => {
+                if (!response.ok) {
+                    throw Error("Network request failed")
+                }
+                return response
+                })
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log(data.items)
+                const res = data.items
+                const posts = res.filter(item => item.categories.length > 0)
+                setMediumPosts(posts)
+                setRequestFailed(true)
+            })
+    }
+
+    useEffect(() => {
+        apiFetch()
+    })
+
+
+
     return (
         <div>
             <h3>Blogs</h3>
