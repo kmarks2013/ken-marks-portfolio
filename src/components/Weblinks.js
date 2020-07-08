d57fb7a5f45e010d3626a047a0f7209ec9b79a0c
kmarks2013 <kmarks2013@gmail.com>
2020-07-08 14:29:01 -0400

    adds weblink component

---
+++ src/components/Weblinks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Weblinks = () => {
+    return (
+        <div className='weblinks'>
+            <p>LinkedIn</p>
+            <p>Github</p>
+            <p>Twitter</p>
+            <i className="fa fa-medium"></i>
+            <p>Medium</p>
+        </div>
+    )
+}
