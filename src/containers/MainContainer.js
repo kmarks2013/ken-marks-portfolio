f0ffdcd9b2ed4b4b0f863c2776dfff0cf0a9418f
kmarks2013 <kmarks2013@gmail.com>
2020-07-08 14:29:43 -0400

    imports twitterfeed and weblink components

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { AboutMe } from '../components/AboutMe'
+import { Weblinks } from '../components/Weblinks'
+import { TwitterFeed } from '../components/TwitterFeed'
 
 export const MainContainer = () => {
     return (
         <div>
             <AboutMe/>
+            <TwitterFeed />
+            <Weblinks />
         </div>
     )
 }
