31f862f142ebe6cea987a06ef70c6374f30dd20b
kmarks2013 <kmarks2013@gmail.com>
2020-07-08 19:42:40 -0400

    addsd blog carousel container

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,13 +4,15 @@ import { Portfolio } from '../components/Portfolio'
 import { Blogs } from '../components/Blogs'
 import { TwitterFeed } from '../components/TwitterFeed'
 import { Weblinks } from '../components/Weblinks'
+import {BlogCarousel} from './BlogCarousel'
 
 export const MainContainer = () => {
     return (
         <div>
             <AboutMe />
             {/* <Portfolio /> */}
-            <Blogs />
+            {/* <Blogs /> */}
+            <BlogCarousel />
             <TwitterFeed />
             <Weblinks />
         </div>
