9ba59972ae84cdc51e37878a693ac1978c03dd95
kmarks2013 <kmarks2013@gmail.com>
2020-07-09 10:33:18 -0400

    fixes indentatiion and updates loading state

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -7,11 +7,12 @@ export const Blogs = () => {
         
     useEffect(() => {
         fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kmarks2013")
-        .then(res => res.json())
-        .then( data => {
-            setMediumPosts(data.items)
-        })
-        .catch(err => console.log(err))
+            .then(res => res.json())
+            .then( data => {
+                setMediumPosts(data.items)
+                setIsLoading(false)
+            })
+            .catch(err => console.log(err))
     }, [])
 
     console.log(mediumPosts)
