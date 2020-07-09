de91fd41b1532cc6e35422f4c08857627beff5f3
kmarks2013 <kmarks2013@gmail.com>
2020-07-09 10:23:37 -0400

    removes blog carousel import and call and uncommetns blogs and portfolio

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,15 +4,13 @@ import { Portfolio } from '../components/Portfolio'
 import { Blogs } from '../components/Blogs'
 import { TwitterFeed } from '../components/TwitterFeed'
 import { Weblinks } from '../components/Weblinks'
-import {BlogCarousel} from './BlogCarousel'
 
 export const MainContainer = () => {
     return (
         <div>
             <AboutMe />
-            {/* <Portfolio /> */}
-            {/* <Blogs /> */}
-            <BlogCarousel />
+            <Portfolio />
+            <Blogs />
             <TwitterFeed />
             <Weblinks />
         </div>
