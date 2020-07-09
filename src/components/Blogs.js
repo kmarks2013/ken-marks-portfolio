920bbf5435c6956604eac278f945850ae3681c81
kmarks2013 <kmarks2013@gmail.com>
2020-07-09 10:33:00 -0400

    adds lodaing state and removes old api call

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -3,24 +3,8 @@ import BlogCard from './BlogCard'
 
 export const Blogs = () => {
     const [mediumPosts, setMediumPosts] = useState([])
-
-    const apiFetch = () => {
-        fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kmarks2013")
-            .then(response => {
-                if (!response.ok) {
-                    throw Error("Network request failed")
-                }
-                return response
-                })
-            .then((res) => res.json())
-            .then((data) => {
-                // console.log(data.items)
-                const res = data.items
-                const posts = res.filter(item => item.categories.length > 0)
-                setMediumPosts(posts)
-            })
-    }
-
+    const [isLoading, setIsLoading] = useState(true)
+        
     useEffect(() => {
         fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kmarks2013")
         .then(res => res.json())
