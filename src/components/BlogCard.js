6b6ab6b5d411f163f8018f45002f0f36834d62d6
kmarks2013 <kmarks2013@gmail.com>
2020-07-10 12:22:48 -0400

    updates blog card to render my way

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,35 +1,31 @@
 import React from 'react'
+import {BlogImage} from './blogcard/BlogImage'
+const moment = require('moment')
 
-
-export default function BlogCard({active, mediumPosts}) {
-    const tagToText = (node) => {
-        let tag = document.createElement('div')
-        tag.innerHTML = node
-        node = tag.innerText
-        return node
-      }
-
-      const shortenText = (text,startingPoint ,maxLength) => {
-        return text.length > maxLength?
-        text.slice(startingPoint, maxLength):
-        text
-      }
+export default function BlogCard({blog}) {
+    console.log(blog)
 
     return (
-        <section>
-            {
+        <section className='blog-card' >
+            <BlogImage src={blog.thumbnail}/>
+            <a className='blog-title' href={blog.link}> {blog.title}</a>
+            <p>
+                    Published:{" "}
+                    {moment(blog.pubDate).format(" MMMM Do YYYY")}
+            </p>
+            {/* {
                 mediumPosts.map((s, index) =>
                     <div className={index === active ? 'active' : 'inactive'} key={index}>
                         <img src={s.thumbnail} style={{width:'50px'}}/>
                         <div>
                         <h5 className='card-title post-title'>{s.title}</h5>
-                        {/* <p>{s.description}</p> */}
+                        <p>{s.description}</p>
                         <p className='card-text post-preview'> {'..' + `${shortenText(tagToText(s.description), 60, 200)}` + '...'}</p>
                         <a href={s.link} className="btn btn-link-grey">Read this article on Medium.com</a>
                         </div>
                     </div>
                 )
-            }
+            } */}
        </section>
     )
 }
