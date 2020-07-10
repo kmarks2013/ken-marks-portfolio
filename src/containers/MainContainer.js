cf52497b9b4ca37582ff0f558976ca47b591ac51
kmarks2013 <kmarks2013@gmail.com>
2020-07-10 12:25:48 -0400

    adds in navbar and blogcontainer

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,13 +4,16 @@ import { Portfolio } from '../components/Portfolio'
 import { Blogs } from '../components/Blogs'
 import { TwitterFeed } from '../components/TwitterFeed'
 import { Weblinks } from '../components/Weblinks'
+import { BlogContainer } from './BlogContainer'
+import NavBar from '../components/NavBar'
 
 export const MainContainer = () => {
     return (
         <div>
+            <NavBar/>
             <AboutMe />
             <Portfolio />
-            <Blogs />
+            <BlogContainer/>
             <TwitterFeed />
             <Weblinks />
         </div>
