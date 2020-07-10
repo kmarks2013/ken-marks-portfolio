acbb9fa76bb095d4a118494f4b9ee3cc0ad9e8bc
kmarks2013 <kmarks2013@gmail.com>
2020-07-10 12:24:00 -0400

    makes changes to blogs to render blog card and removes fetch"

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,26 +1,16 @@
 import React, {useState, useEffect}  from 'react'
 import BlogCard from './BlogCard'
 
-export const Blogs = () => {
-    const [mediumPosts, setMediumPosts] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-        
-    useEffect(() => {
-        fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kmarks2013")
-            .then(res => res.json())
-            .then( data => {
-                setMediumPosts(data.items)
-                setIsLoading(false)
-            })
-            .catch(err => console.log(err))
-    }, [])
+export const Blogs = ({blogList}) => {
+    console.log(blogList)
 
-    console.log(mediumPosts)
-
+    const renderBlogs = blogList.map(blog =>
+       <BlogCard blog={blog}/>
+        )
 
     return (
-        <div>
-            <h3>Blogs</h3>
+        <div className='bloglist'>
+            {renderBlogs}
         </div>
     )
 }
