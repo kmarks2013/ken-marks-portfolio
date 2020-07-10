e966263d4ed0063c5370a3360f8f07c0fc140f57
kmarks2013 <kmarks2013@gmail.com>
2020-07-10 12:25:17 -0400

    adds classnames to links and images

--- src/components/Weblinks.js
+++ src/components/Weblinks.js
@@ -4,16 +4,16 @@ import 'font-awesome/css/font-awesome.min.css'
 export const Weblinks = () => {
     return (
         <div className='weblinks'>
-            <a href="https://www.linkedin.com/in/kmarks2013/">
-            <i class="fa fa-linkedin" aria-hidden="true"></i>
+            <a className='link' href="https://www.linkedin.com/in/kmarks2013/" target="_blank">
+            <i className="fa fa-linkedin" aria-hidden="true"></i>
             </a>
-            <a href="https://github.com/kmarks2013">
+            <a className='link' href="https://github.com/kmarks2013" target="_blank">
             <i className="fa fa-github" aria-hidden="true"></i>
             </a>
-            <a href="https://twitter.com/kmarks2013">
+            <a className='link' href="https://twitter.com/kmarks2013" target="_blank">
             <i className="fa fa-twitter" aria-hidden="true"></i>
             </a>
-            <a href="https://medium.com/kmarks2013">
+            <a className='link' href="https://medium.com/@kmarks2013" target="_blank">
             <i className="fa fa-medium"></i>
             </a>
         </div>
