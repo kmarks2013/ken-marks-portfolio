bfb28098cbbcfb5d6fc433e408cacf1d044ced8d
kmarks2013 <kmarks2013@gmail.com>
2020-07-10 12:26:34 -0400

    adds Navbar Component

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className="nav">
+            <h1 className="nav-header">Kenny Marks</h1>
+            <div className='nav-body'>
+                <p>About</p>
+                <p>Portfolio</p>
+                <p>Blog</p>
+                <p>Twitter</p>
+                <p>Contact</p>
+            </div> 
+        </div>
+    )
+}
+
+export default NavBar
