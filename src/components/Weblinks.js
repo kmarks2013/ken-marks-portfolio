3fdebe7b57e57ef024627b0490123e093537daa4
kmarks2013 <kmarks2013@gmail.com>
2020-07-19 17:01:20 -0400

    adds rel line to remove error when using nav links

--- src/components/Weblinks.js
+++ src/components/Weblinks.js
@@ -4,16 +4,16 @@ import 'font-awesome/css/font-awesome.min.css'
 export const Weblinks = () => {
     return (
         <div className='weblinks'>
-            <a className='link' href="https://www.linkedin.com/in/kmarks2013/" target="_blank">
+            <a className='link' href="https://www.linkedin.com/in/kmarks2013/" target="_blank" rel="noopener noreferrer">
             <i className="fa fa-linkedin" aria-hidden="true"></i>
             </a>
-            <a className='link' href="https://github.com/kmarks2013" target="_blank">
+            <a className='link' href="https://github.com/kmarks2013" target="_blank" rel="noopener noreferrer">
             <i className="fa fa-github" aria-hidden="true"></i>
             </a>
-            <a className='link' href="https://twitter.com/kmarks2013" target="_blank">
+            <a className='link' href="https://twitter.com/kmarks2013" target="_blank" rel="noopener noreferrer">
             <i className="fa fa-twitter" aria-hidden="true"></i>
-            </a>
-            <a className='link' href="https://medium.com/@kmarks2013" target="_blank">
+            </a >
+            <a className='link' href="https://medium.com/@kmarks2013" target="_blank" rel="noopener noreferrer">
             <i className="fa fa-medium"></i>
             </a>
         </div>
