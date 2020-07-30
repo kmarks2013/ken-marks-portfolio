cd43977b5379e4bf789e6bea796b8fb9ee6c00de
kmarks2013 <kmarks2013@gmail.com>
2020-07-30 16:44:50 -0400

    removes component renders and renders content container instead

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import { AboutMe } from '../components/AboutMe'
-import { Portfolio } from './Portfolio'
-import { Blogs } from '../components/Blogs'
-import { TwitterFeed } from '../components/TwitterFeed'
-import { Weblinks } from '../components/Weblinks'
-import { BlogContainer } from './BlogContainer'
 import NavBar from './NavBar'
+import ContentContainer from './ContentContainer'
 
 export const MainContainer = () => {
     return (
         <div>
             <NavBar/>
-            <AboutMe />
-            <Portfolio />
-            <BlogContainer/>
-            <TwitterFeed />
-            <Weblinks />
+            <ContentContainer />
         </div>
     )
 }
