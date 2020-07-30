f774191172e2ae34bde7bb14a269850a5fce4632
kmarks2013 <kmarks2013@gmail.com>
2020-07-30 16:39:43 -0400

    adds content container to  add content components

---
+++ src/containers/ContentContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {AboutMe} from '../components/AboutMe'
+import { BlogContainer } from './BlogContainer'
+
+function ContentContainer() {
+    return (
+        <div>
+            <AboutMe/>
+            <BlogContainer/>
+
+        </div>
+    )
+}
+
+export default ContentContainer
