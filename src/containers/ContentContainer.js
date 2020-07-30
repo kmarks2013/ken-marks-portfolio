0fb0b274d9a43a26baf78053924801e9310e095f
kmarks2013 <kmarks2013@gmail.com>
2020-07-30 16:43:30 -0400

    imports rest of compoents

--- src/containers/ContentContainer.js
+++ src/containers/ContentContainer.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {AboutMe} from '../components/AboutMe'
 import { BlogContainer } from './BlogContainer'
+import { Portfolio } from './Portfolio'
+import { TwitterFeed } from '../components/TwitterFeed'
+import { Weblinks } from '../components/Weblinks'
 
 function ContentContainer() {
     return (
