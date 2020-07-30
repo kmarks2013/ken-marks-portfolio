9de5f42e57002a270b5fa97ba281d0868117f2ba
kmarks2013 <kmarks2013@gmail.com>
2020-07-30 16:35:22 -0400

    adds SectionHead file

---
+++ src/components/SectionHead.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SectionHead = () => {
+    return (
+        <div className="nav-section-head">
+            <h1 className="nav-header">Kenny Marks</h1>
+            <h3 className="nav-header-desc"> Fullstack Software Engineer</h3>
+        </div>
+    )
+}
+
+export default SectionHead
